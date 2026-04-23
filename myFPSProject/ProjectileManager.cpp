b4a06d79bd87176bf6d9c22c798f750d5d1b794e
2571574 <mit2571574@stu.o-hara.ac.jp>
2026-04-24 03:38:05 +0900

    4/23

--- myFPSProject/ProjectileManager.cpp
+++ myFPSProject/ProjectileManager.cpp
@@ -20,11 +20,11 @@ void ProjectileManager::Update()
 	for (int i = (int)projectiles.size() - 1; i >= 0; i--){
 		projectiles[i]->Update();	
 	}
-		//生存タグのない弾を消去する
-		projectiles.erase(std::remove_if(projectiles.begin(), projectiles.end(),
-			[](const std::unique_ptr<BaseProjectile>& p) {return !p->GetAlive(); }),
-			projectiles.end()
-		);
+
+	//生存タグのない弾を消去する
+	std::erase_if(projectiles, [](const auto& p) {
+		return !p->GetAlive();
+	});
 }
 
 void ProjectileManager::Draw()
